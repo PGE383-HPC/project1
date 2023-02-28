f2d8689fba74483b01b5667435e6f6c5c49e6092
John T. Foster <johntfosterjr@gmail.com>
2023-02-28 09:47:24 -0600

    First commit

--- src/project1.jl
+++ src/project1.jl
@@ -15,58 +15,13 @@
 # limitations under the License.
 module project1
 
-using DifferentialEquations
-using LaTeXStrings
-using Plots
 
-
-function lorenz!(du, u, params, t)
-    σ, β, ρ = params
-    x, y, z = u
-
-    du[1] = σ * (y - x)
-    du[2] = x * (ρ - z) - y
-    du[3] = x * y - β * z
-    nothing
-end
-
-function lorenz_solver(x₀::Real, ρ::Real)
-    prob = ODEProblem(lorenz!, [x₀; 1.0; 0.0],  (0.0, 100.0), [10.0, 8.0/3.0, ρ])
-    solve(prob)
-end
-
-function plot3(ρ::Real)
-    theme(:default, xformatter= x -> L"%$x", yformatter= x -> L"%$x")
-    t = 0.0:0.01:100.0
-    sol1 = lorenz_solver(0.0, ρ)(t)
-    sol2 = lorenz_solver(1e-5, ρ)(t)
-
-    x₁ = getindex.(sol1.u, 1)
-    x₂ = getindex.(sol2.u, 1)
-    z₁ = getindex.(sol1.u, 3)
-
-    p1 = plot(t, [x₁, x₂], label=nothing, xlabel=L"t", ylabel=L"x_1, x_2", 
-              title=L"\rho = %$ρ", xformatter= x -> L"%$x", yformatter= x -> L"%$x")
-    p2 = plot(t, abs.(x₁ .- x₂), label=nothing, xlabel=L"t", ylabel=L"\vert x_1 - x_2\vert")
-    p3 = plot(x₁, z₁, label=nothing, xlabel=L"x_1", ylabel=L"z_1")
-
-    (p1, p2, p3)
+function lorenz_solver(x₀, ρ)
+    #return the ODESolution
 end
 
 function lorenz_plot()
-    p1, p2, p3 = project1.plot3(14.0)
-    p4, p5, p6 = project1.plot3(28.0)
-    p1 = plot!(p1, xlabel="", xformatter= _ -> "")
-    p4 = plot!(p4, xlabel="", ylabel="", xformatter= _ -> "", yformatter= _ -> "")
-    p5 = plot!(p5, ylabel="", yformatter= _ -> "")
-    p6 = plot!(p6, ylabel="", yformatter= _ -> "")
-    p7 = plot(p1, p4, p2, p5, layout=(2,2), link=:both)
-    p8 = plot(p3, p6, layout=(1,2), link=:y)
-
-    l = @layout [
-        a{0.6666h}
-        b{0.3333h}]
-    plot(p7, p8, layout=l)
+    #return the final plot
 end
 
 export lorenz_solver, lorenz_plot
